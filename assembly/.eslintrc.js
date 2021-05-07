71a1ed34fb4cfaf0615d443bc48de1beacdb92d0
egs33 <egs33.bicycle@gmail.com>
2021-05-07 23:07:36 +0900

    Fix eslint config

--- assembly/.eslintrc.js
+++ assembly/.eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   root: true,
   extends: '@egs33/eslint-config/typescript-base',
   parserOptions: {
-    project: './assembly/tsconfig.json',
+    project: './tsconfig.json',
   },
+  rules: {
+    '@typescript-eslint/prefer-for-of': 'off',
+  }
 };
