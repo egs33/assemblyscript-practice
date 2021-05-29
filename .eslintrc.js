a84c1156842fe8e5989a79c6e90c9519be91ebbd
egs33 <egs33.bicycle@gmail.com>
2021-05-29 23:28:23 +0900

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@egs33/eslint-config/typescript-browser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
+  extends: '@egs33',
+  overrides: [
+    {
+      files: '*.ts',
+      extends: '@egs33/eslint-config/typescript-node',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
 };
